fddb9137305a198b295bbd5141b9fa5919f76ae3
Erik Andersson <erik.anderzzon@gmail.com>
2021-01-29 10:33:55 +0100

    Schedule

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require("firebase-functions");
+const fetch = require("node-fetch");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,42 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// Auth trigger
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  console.log("User created: ", user.uid);
+
+  const message = {
+    to: "ExponentPushToken[DOu9c1KSwIB35nu2-eZuSs]",
+    body: "Test Notification",
+  };
+
+  fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+});
+
+// Scheduled function - working!
+// exports.scheduledFunction =
+// functions.pubsub.schedule("every 60 minutes").onRun((context) => {
+//   console.log("This will be run every 5 minutes!");
+//   const message = {
+//     to: "ExponentPushToken[DOu9c1KSwIB35nu2-eZuSs]",
+//     body: "Repeated Notification",
+//   };
+
+//   fetch("https://exp.host/--/api/v2/push/send", {
+//     method: "POST",
+//     headers: {
+//       "Accept": "application/json",
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(message),
+//   });
+//   return null;
+// });
